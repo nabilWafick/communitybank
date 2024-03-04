beb9f91c728560a4b13176399424680bef4fae64
nabilWafick <nabilsoumanou7@gmail.com>
2024-03-04 20:12:20 +0100

    begin forecast inserting

---
+++ lib/models/rpc/forecasts/forecasts_rpc.model.dart
@@ -0,0 +1,21 @@
+class CustomersTypesRPC {
+  static const String functionName = 'get_customers_types';
+  static const String productId = 'id_produit';
+  static const String productName = 'nom_produit';
+  static const String productPurcharsePrice = 'prix_achat_produit';
+  static const String forecastsNumber = 'prevision_nombre';
+  static const String forecastsAmount = 'prevision_montant';
+  static const String deservingProductNumber = 'nombres_produits_merites';
+  static const String typeProductAmount = 'nombres_produits_types';
+  static const String collectorsIds = 'ids_charges_compte';
+  static const String collectors = 'charges_compte';
+  static const String customersAccountsIds = 'ids_comptes_clients';
+  static const String customersIds = 'ids_clients';
+  static const String customers = 'clients';
+  static const String customersCardsLabels = 'libelles_cartes';
+  static const String customersCardsTypesNumbers = 'nombres_types_cartes';
+  static const String customersCardsSettlementsTotals =
+      'totaux_reglements_cartes';
+  static const String customersCardsSettlementsAmounts =
+      'montants_reglements_cartes';
+}
